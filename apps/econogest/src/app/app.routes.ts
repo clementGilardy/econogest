501cac2bb3d99e1e881a9da435416c62215c077f
clementGilardy <clement.gilardy@outlook.fr>
2024-11-01 16:35:38 +0100

    feat(router): add routes to the router

--- apps/econogest/src/app/app.routes.ts
+++ apps/econogest/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@econogest/dashboard').then((m) => m.DashboardComponent),
+    pathMatch: 'full',
+  },
+  {
+    path: 'transactions',
+    loadComponent: () =>
+      import('@econogest/transactions').then((m) => m.TransactionsComponent),
+  },
+];
