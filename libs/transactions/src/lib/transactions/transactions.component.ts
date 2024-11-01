73d5f16b54262e48f85248092fbe804d3fdb6db3
clementGilardy <clement.gilardy@outlook.fr>
2024-11-01 16:22:05 +0100

    feat(monorepo) add ui and transaction monorepo

---
+++ libs/transactions/src/lib/transactions/transactions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-transactions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transactions.component.html',
+  styleUrl: './transactions.component.css',
+})
+export class TransactionsComponent {}
